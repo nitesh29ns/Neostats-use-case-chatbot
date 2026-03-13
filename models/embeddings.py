da67c65022c2ec4778a5de0f1bc71ee0fbb01e17
Nitesh Sharma <niteshsharma29.ns@gmail.com>
2026-03-14 03:09:07 +0530

    Remove environment variable setting
    Removed the setting of the NOMIC_API_KEY from environment variables.

--- models/embeddings.py
+++ models/embeddings.py
@@ -5,9 +5,6 @@ os.environ["NOMIC_API_KEY"] = st.secrets["NORMIC_EMBEDDING"]
 from langchain_nomic import NomicEmbeddings  
 from config.config import NORMIC_EMBEDDING, EMBEDDING_MODEL_NAME
 
-os.environ["NOMIC_API_KEY"] = st.secrets["NOMIC_API_KEY"]
-
-
 def get_embedding_funcation():
         embedding = NomicEmbeddings(
             model=EMBEDDING_MODEL_NAME,)
