4e4d4509fb48f10a318885e47a873c8eeae30f8e
nitesh29ns <94296741+nitesh29ns@users.noreply.github.com>
2026-03-14 02:50:52 +0530

    small change

--- utils/preprocessing_query.py
+++ utils/preprocessing_query.py
@@ -1,6 +1,9 @@
+import nltk
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 from utils.logger import lg
+
+nltk.download('stopwords')
 
 lg.info("=============================================================== PREPROCESSING ===============================================================")
 
