2519bcec29fe46208359e5b7d051f3d1cd358afc
nitesh29ns <94296741+nitesh29ns@users.noreply.github.com>
2026-03-14 03:00:16 +0530

    small change

--- models/embeddings.py
+++ models/embeddings.py
@@ -1,5 +1,11 @@
+import os
+import streamlit as st
+os.environ["NOMIC_API_KEY"] = st.secrets["NOMIC_API_KEY"]
+
 from langchain_nomic import NomicEmbeddings  
 from config.config import NORMIC_EMBEDDING, EMBEDDING_MODEL_NAME
+
+os.environ["NOMIC_API_KEY"] = st.secrets["NOMIC_API_KEY"]
 
 
 def get_embedding_funcation():
